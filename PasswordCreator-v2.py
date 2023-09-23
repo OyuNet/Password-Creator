d426b6465a224279c928fddfdea9cb39e3ac76d8
OyuNet <oyunarda662@gmail.com>
2023-09-23 13:41:43 +0300

    ayarlar kısmını yazmaya başladım. bir ara bitiririm ama ne zaman bilinmez.

--- PasswordCreator-v2.py
+++ PasswordCreator-v2.py
@@ -48,5 +48,37 @@ while True:
             else:
                 print('Şifreler başarıyla oluşturulmuştur.')
                 break
+    
+    elif "Ayarlar" in MainSelection:
+        while True:
+            SettingsSelection = select(
+            "Ayarlara hoş geldiniz. Değiştirmek istediğiniz ayarı seçiniz.",
+            choices = ['Dil Tercihi',
+                       'Toplu Kayıt Dosyası',
+                       'Geri']    
+            ).ask()
+            if "Dil Tercihi" in SettingsSelection:
+                while True:
+                    LangSelection = select(
+                    "Dilinizi tercih ediniz",
+                    choices = ['Türkçe',
+                                'İngilizce',
+                                'Geri']
+                    ).ask()
+                    if "Türkçe" in LangSelection:
+                        print("Diliniz Türkçe olarak ayarlandı.")
+                        break
+                    elif "İngilizce" in LangSelection:
+                        print("Diliniz İngilizce olarak ayarlandı.")
+                        break
+                    elif "Geri" in LangSelection:
+                        break
+                
+            elif "Toplu Kayıt Dosyası" in SettingsSelection:
+                fileInput = str(input("Yeni dosya adını giriniz: "))
+                print("Yeni dosya adınız: " + fileInput + ".txt")
+                
+            elif "Geri" in SettingsSelection:
+                break        
     elif "Çıkış" in MainSelection:
         break            
